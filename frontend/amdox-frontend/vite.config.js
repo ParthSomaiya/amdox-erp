01020bc84eaf1e9ae78811052eac2550fdfac4f3
Parth Somaiya <parthsomaiya2004@gmail.com>
2026-05-24 16:33:16 +0530

    Update vite.config.js

--- frontend/amdox-frontend/vite.config.js
+++ frontend/amdox-frontend/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
 
-  optimizeDeps: {
-    include: ["react", "react-dom"]
-  }
+  resolve: {
+    alias: {
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    },
+  },
 });
